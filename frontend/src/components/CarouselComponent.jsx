38b72ebe104afaf6ed10122973a18e18e5357a85
Tirthendu <tirthendusekhar@gmail.com>
2026-08-28 23:08:36 +0530

    Modified imports

--- frontend/src/components/CarouselComponent.jsx
+++ frontend/src/components/CarouselComponent.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
-import clothingImg from "../assets/clothing.png";
-import electronicsImg from "../assets/electronics.png";
-import fitnessImg from "../assets/fitness.png";
-import arrow from "../assets/carouselArrow.svg";
+import clothingImg from "/src/assets/clothing.png";
+import electronicsImg from "/src/assets/electronics.png";
+import fitnessImg from "/src/assets/fitness.png";
+import arrow from "/src/assets/carouselArrow.svg";
 const images = [
   { src: clothingImg, alt: "Clothing" },
   { src: electronicsImg, alt: "Electronics" },
